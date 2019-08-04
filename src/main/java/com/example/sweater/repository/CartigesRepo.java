10fcd9ca4edff3d96284668bad2b1d062be122fa
AndreY <Shutlegend@yandex.ru>
2019-08-04 16:19:53 +0300

    start application

--- src/main/java/com/example/sweater/repository/CartigesRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.sweater.repository;
-
-import domain.cartriges;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CartigesRepo extends CrudRepository<cartriges, Long> {
-}
